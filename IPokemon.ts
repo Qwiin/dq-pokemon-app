ac8571d8a047f470fecdd9b32f271686812810a8
Devon Quinn <devon.q.traver@gmail.com>
2024-03-26 22:28:50 -0400

    Revert "Fixed typo in interface"
    This reverts commit cb98da9bb6504c14ce72b1893ae0a5643440a217.

--- IPokemon.ts
+++ IPokemon.ts
@@ -47,7 +47,7 @@ export interface IPokemon {
   id: number;
   is_default: boolean;
   location_area_encounters: string;
-  moves: IPokemonMove[],
+  moves: PokemonMove[],
   name: string,
   order: number,
   past_abilities: {
